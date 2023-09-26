0880fc2b9bdf5e5f8d6d353a3158f46db405ba74
Luiz M. Faria <maltezfaria@gmail.com>
2023-09-26 17:52:40 +0200

    simplify

--- paper/makefigures.jl
+++ paper/makefigures.jl
@@ -2,7 +2,7 @@ using Pkg
 using Logging
 
 dir_paper = @__DIR__
-Pkg.activate(joinpath(dir_paper, "."))
+Pkg.activate(dir_paper)
 Pkg.instantiate()
 
 dir_scripts = joinpath(dir_paper, "scripts")
@@ -16,7 +16,7 @@ dir_figures = joinpath(dir_paper, "figures")
 dir_animations = joinpath(dir_paper, "animations")
 
 # map script file to figure file
-script2fig = Dict(
+script2fig = [
     "wavemaker_solution.jl" => "fig2a",
     "wavemaker_pml_convergence.jl" => "fig2b",
     "modes_vary_impedance.jl" => "fig3",
@@ -29,22 +29,26 @@ script2fig = Dict(
     "double_piercing_scattering_mesh_convergence.jl" => "fig9",
     "step_scattering.jl" => "fig10",
     "double_piercing_eigenvalue.jl" => "fig11",
-)
+]
 
 # generate all figures
 for (file, fig) in script2fig
-    @info "Executing $file to generate $fig"
-    full_path = joinpath(dir_scripts, file)
-    log_file  = joinpath(dir_logs, "$(fig).log")
-    io        = open(log_file, "w+")
-    logger    = ConsoleLogger(io)
-    # wrap each include around a module to avoid namespace pollution and
-    # conflicts
-    with_logger(logger) do
-        @eval module $(gensym(file))
-        include($full_path)
+    @info "Executing $file to generate $fig..."
+    t = @elapsed begin
+        full_path = joinpath(dir_scripts, file)
+        log_file  = joinpath(dir_logs, "$(fig).log")
+        open(log_file, "w+") do io
+            logger    = ConsoleLogger(io)
+            # wrap each include around a module to avoid namespace pollution and
+            # conflicts
+                with_logger(logger) do
+                    @eval module $(gensym(file))
+                    include($full_path)
+                    end
+                end
+            flush(io)
+            close(io)
         end
     end
-    flush(io)
-    close(io)
+    @info "|---finished in $t seconds"
 end
